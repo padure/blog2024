dc8ff1fa6ac8e62f23d17a061938d239b6dc6d99
padure <paduregheorghe7@gmail.com>
2024-02-02 09:28:51 +0200

    Instalat Spatie, Breeze si configurat rolurile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,45 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * List of applications to add.
+     */
+    private $permissions = [
+        'role-list',
+        'role-create',
+        'role-edit',
+        'role-delete'
+    ];
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        foreach ($this->permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create admin User and assign the role to him.
+        $user = User::create([
+            'name' => 'Padure Gheorghe',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('Secret2024')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
     }
 }
