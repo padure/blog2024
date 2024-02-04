a3036eab16044670ebbf8c71db4b01bbc8505c20
padure <paduregheorghe7@gmail.com>
2024-02-04 15:24:45 +0200

    Relatiile modelelor

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -14,4 +15,14 @@ class Comment extends Model
         'article_id',
         'user_id'
     ];
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
